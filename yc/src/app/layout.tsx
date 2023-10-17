f844d5d625744d6f84230a6bf216dd9f25c3ab50
AzarAhmadov <82292818+AzarAhmadov@users.noreply.github.com>
2023-10-17 11:55:10 +0400

    Layout

---
+++ yc/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import '../css/globals.scss'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ yc/src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home: React.FC = () => {
+  return (
+    <main>
+      page
+    </main>
+  )
+}
+
+export default Home
