340fcb75c261096551be6fbbb2d5edff1910707a
AzarAhmadov <82292818+AzarAhmadov@users.noreply.github.com>
2023-10-18 15:11:14 +0400

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <title>YouTube</title>
+        <link rel="icon" href="page_logo_jpeg"></link>
         <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600;700&family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
       </head>
